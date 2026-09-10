19f0a5ffda32cccba1f19c68d0ae33325c63f3a0
9Lotus9 <9lotusnine@gmail.com>
2026-09-10 10:25:49 +0800

    刷新桌面应用缓存

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-const CACHE = "energy-reminder-v2";
+const CACHE = "energy-reminder-v3";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon-192.svg"];
-self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
 self.addEventListener("push", event => {
   const data = event.data?.json() || { title: "梦幻消除战体力已满", body: "体力已恢复至 100 点，快去闯关吧！" };
